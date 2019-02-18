d0d6de6464257c3f859e73d69540ea107fcb52ac
Skipping903 <albieluk000@yahoo.com>
2019-02-18 07:46:51 -0600

    HW3 Commit
    Animation and crouching/jumping

--- Assets/Scripts/FPSController.cs
+++ Assets/Scripts/FPSController.cs
@@ -23,12 +23,26 @@ public class FPSController : MonoBehaviour
     private CharacterController charController;
     private Vector3 moveDirection = Vector3.zero;
 
+    public LayerMask groundLayer;
+    private float rayDistance;
+    private float default_ControllerHeight;
+    private Vector3 default_CamPos;
+    private float camHeight;
+
+    private FPSPlayerAnimations playerAnimation;
+
     void Start()
     {
         this.firstPersonView = this.transform.Find("FPS View").transform;
         this.charController = this.GetComponent<CharacterController>();
         this.speed = this.walkSpeed;
         this.is_Moving = false;
+
+        this.rayDistance = this.charController.height * 0.5f + this.charController.radius;
+        this.default_ControllerHeight = this.charController.height;
+        this.default_CamPos = this.firstPersonView.localPosition;
+
+       this.playerAnimation = this.GetComponent<FPSPlayerAnimations>();
     }
 
     void Update()
@@ -81,10 +95,14 @@ public class FPSController : MonoBehaviour
 
         if (this.is_Grounded)
         {
+            this.PlayerCrouchingAndSprinting();
+
             this.moveDirection = new Vector3(this.inputX * this.inputModifyFactor, -this.antiBumpFactor,
                 inputY * inputModifyFactor);
 
             this.moveDirection = this.transform.TransformDirection(moveDirection) * this.speed;
+
+            this.PlayerJump();
         }
         this.moveDirection.y -= this.gravity * Time.deltaTime;
 
@@ -92,5 +110,116 @@ public class FPSController : MonoBehaviour
             & CollisionFlags.Below) != 0;
 
         this.is_Moving = this.charController.velocity.magnitude > 0.15f;
+
+        this.HandleAnimations();
+    }
+
+    void PlayerCrouchingAndSprinting()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            if (!is_Crouching)
+            {
+                this.is_Crouching = true;
+            }
+            else
+            {
+                if (CanGetUp())
+                {
+                    this.is_Crouching = false;
+                }
+            }
+            this.StopCoroutine(MoveCameraCrouch());
+            this.StartCoroutine(MoveCameraCrouch());
+        }
+        if (this.is_Crouching)
+        {
+            this.speed = this.crouchSpeed;
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                this.speed = this.runSpeed;
+            }
+            else
+            {
+                this.speed = this.walkSpeed;
+            }
+        }
+        this.playerAnimation.PlayerCrouch(is_Crouching);
+    }
+
+    bool CanGetUp()
+    {
+        Ray groundRay = new Ray(transform.position, transform.up);
+        RaycastHit groundHit;
+
+        if (Physics.SphereCast(groundRay, charController.radius + 0.05f,
+            out groundHit, rayDistance, groundLayer))
+        {
+
+            if (Vector3.Distance(transform.position, groundHit.point) < 2.3f)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    IEnumerator MoveCameraCrouch()
+    {
+        this.charController.height = is_Crouching ? default_ControllerHeight / 1.5f : default_ControllerHeight;
+        this.charController.center = new Vector3(0f, charController.height / 2f, 0f);
+
+        camHeight = is_Crouching ? default_CamPos.y / 1.5f : default_CamPos.y;
+
+        while (Mathf.Abs(camHeight - firstPersonView.localPosition.y) > 0.01f)
+        {
+
+            this.firstPersonView.localPosition = Vector3.Lerp(firstPersonView.localPosition,
+                new Vector3(this.default_CamPos.x, this.camHeight, this.default_CamPos.z),
+                Time.deltaTime * 11f);
+
+            yield return null;
+        }
+    }
+
+    void PlayerJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            if (is_Crouching)
+            {
+
+                if (CanGetUp())
+                {
+                    this.is_Crouching = false;
+
+                     this.playerAnimation.PlayerCrouch(is_Crouching);
+
+                    this.StopCoroutine(MoveCameraCrouch());
+                    this.StartCoroutine(MoveCameraCrouch());
+                }
+
+            }
+            else
+            {
+                this.moveDirection.y = this.jumpSpeed;
+            }
+        }
+    }
+
+    void HandleAnimations()
+    {
+        this.playerAnimation.Movement(this.charController.velocity.magnitude);
+        this.playerAnimation.PlayerJump(this.charController.velocity.y);
+
+        if (this.is_Crouching && this.charController.velocity.magnitude > 0f)
+        {
+            this.playerAnimation.PlayerCrouchWalk(this.charController.velocity.magnitude);
+        }
     }
 }//class
---
+++ Assets/Scripts/FPSPlayerAnimations.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSPlayerAnimations : MonoBehaviour
+{
+    private Animator anim;
+
+    private string MOVE = "Move";
+    private string VELOCITY_Y = "VelocityY";
+    private string CROUCH = "Crouch";
+    private string CROUCH_WALK = "CrouchWalk";
+
+    void Awake()
+    {
+        this.anim = this.GetComponent<Animator>();
+    }
+
+    public void Movement(float magnitude)
+    {
+        this.anim.SetFloat(MOVE, magnitude);
+    }
+
+    public void PlayerJump(float velocity)
+    {
+        this.anim.SetFloat(VELOCITY_Y, velocity);
+    }
+
+    public void PlayerCrouch(bool isCrouching)
+    {
+        this.anim.SetBool(CROUCH, isCrouching);
+    }
+
+    public void PlayerCrouchWalk(float magnitude)
+    {
+        this.anim.SetFloat(CROUCH_WALK, magnitude);
+    }
+}//class
+
